5661fd40567260719b90d0045696fda805ea6ba1
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-30 04:07:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,15 +15,15 @@
 
 def line(katz_deli)
   
-  if katz_deli.length <= 1
+  if katz_deli.length < 1
     puts "The line is currently empty."
   else
     names = []
     katz_deli.each_with_index do |name, index|
       names.push("#{index + 1}. #{name}")
     end
-  end
     puts "the line is currently: #{names.join(" ")}"
+  end
 end
 
 def take_a_number(katz_deli, name)
