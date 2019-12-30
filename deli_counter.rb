d86e4663b83bcdcfb4a2ad2720212007932f29a2
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-30 04:26:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,6 +25,6 @@ def now_serving(katz_deli)
   if katz_deli.length < 1
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.shift
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
