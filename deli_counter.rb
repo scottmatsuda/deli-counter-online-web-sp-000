e992516eaf1ffa6adbb624721031313653890eff
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-30 03:51:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length < 1
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, index|
+      p "#{index + 1}. #{name}"
+    end
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length < 1
+    katz_deli.push(name)
+  else
+    katz_deli.each_with_index do |name, index|
+      puts "Welcome, #{name}. You are number #{index + 1} in line."
+    end
+  end
+end
