5a91cbf89c8af87e7115a0cf669a1e20dc0aac47
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-30 04:20:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ def take_a_number(katz_deli, name)
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number 1 in line."
   else
+    katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
   end
 end
