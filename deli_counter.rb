7148bf6bad7fb855c22f90b524ce68fda388dbde
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-30 03:53:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-      puts "The line is currently: #{index + 1}. #{name}"
+      puts "The line is currently: " + #{index + 1} + "." + #{name}
     end
   end
 end
