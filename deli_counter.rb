f813cdaa2dfcce2e990d0e5825d75db2a2ad6937
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-30 04:05:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,30 @@
 
-def line(array) # this was the one I figured out
-  if array.length >= 1
-    nuarray = []
-    counter = 1 
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{nuarray.join(" ")}"
-  else
-    puts "The line is currently empty."
-  end
-end
-
-# def line(katz_deli)
-  
-#   if katz_deli.length < 1
-#     puts "The line is currently empty."
+# def line(array) # this was the one I figured out
+#   if array.length >= 1
+#     nuarray = []
+#     counter = 1 
+#     array.each do |name|
+#       nuarray.push("#{counter}. #{name}")
+#       counter += 1 
+#     end 
+#     puts "The line is currently: #{nuarray.join(" ")}"
 #   else
-#     names = []
-#     katz_deli.each_with_index do |name, index|
-#       names << "#{index + 1}. #{name}"
-#     end
+#     puts "The line is currently empty."
 #   end
-#     puts "the line is currently: #{names.join(" ")}"
 # end
+
+def line(katz_deli)
+  
+  if katz_deli.length < 1
+    puts "The line is currently empty."
+  else
+    names = []
+    katz_deli.each_with_index do |name, index|
+      names.push("#{index + 1}. #{name}")
+    end
+  end
+    puts "the line is currently: #{names.join(" ")}"
+end
 
 def take_a_number(katz_deli, name)
   if katz_deli.length < 1
