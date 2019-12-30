8f65cb74c239229ced76b6e8daf210d0cf8d7df2
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-30 04:24:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,8 @@ def take_a_number(katz_deli, name)
   end
 end
 
-def now_serving
-  
+def now_serving(katz_deli)
+  if katz_deli.length < 1
+    puts "There is nobody waiting to be served!"
+  end
 end
